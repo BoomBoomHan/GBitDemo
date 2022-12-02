f632d7a3aab774a06aa5a3bbd734e6f7315ce3f8
26384 <107617689+26384@users.noreply.github.com>
2022-12-03 00:58:26 +0800

    玩家红蓝血量及阵营

---
+++ Assets/Game/Source/Y_PAK/NonScripts/Classes/PlayerManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PlayerManager:MonoBehaviour
+{
+    [SerializeField, DisplayName("蓝方角色")]
+    public GameObject Blue_GameObject;
+    [SerializeField, DisplayName("蓝方血量")]
+    public float Blue_Blood;
+
+
+    [SerializeField, DisplayName("红方角色")]
+    public GameObject Red_GameObject;
+    [SerializeField, DisplayName("红方血量")]
+    public float Red_Blood;
+
+
+
+
+
+    public PlayerManager()
+    {
+        
+    }
+}
