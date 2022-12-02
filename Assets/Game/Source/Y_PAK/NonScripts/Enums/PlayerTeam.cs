a8c8c017094c22ecd882e9a8b2ee170634ef322d
class BoomHan : SpoofMaker <84579735+BoomBoomHan@users.noreply.github.com>
2022-12-02 23:20:27 +0800

    添加代码文件

---
+++ Assets/Game/Source/Y_PAK/NonScripts/Enums/PlayerTeam.cs
@@ -0,0 +1,10 @@
+
+using System.Runtime.Serialization;
+
+public enum EPlayerTeam
+{
+    [EnumMember(Value = "蓝色方")]
+    Blue,
+	[EnumMember(Value = "红色方")]
+	Red,
+}
---
+++ Assets/Game/Source/Y_PAK/NonScripts/FunctionLibraries/GameComputions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameComputions
+{
+    //此条为示例。
+    //public static GetPlayerTeam(PlayerData data)
+    //{
+    //      return data.team;
+    //}
+}
