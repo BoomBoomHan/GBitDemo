3d53f1af87fa1738451ce5fee0da513a1e7ed6be
26384 <107617689+26384@users.noreply.github.com>
2022-12-03 10:58:00 +0800

    计算矩阵值

---
+++ Assets/Game/Source/Y_PAK/NonScripts/FunctionLibraries/RoundDamage.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RoundDamage
+{
+    public static int damage;
+
+    private static int damage_tiles = 0;//岩浆，伤害为0
+    private static int prop_tiles = 0;//道具地砖，伤害为0
+    private static int normal_tiles = 0;
+    public static int x = 0;
+
+    public static int[,] blue;
+    public static int[,] red;
+
+    private static void NormalTile()
+    {
+
+    }
+
+    private static void AddNumber()//先计算3*3中的数
+    {
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                x = blue[i, j] + blue[damage_tiles, damage_tiles] + blue[prop_tiles, prop_tiles];
+            }
+        }
+        
+    } 
+
+    private static void AddMatrix(int[] Blue)//计算2*2的矩阵
+    {
+        AddNumber();
+        for (int i = 0; i < Blue.Length-1; i++)
+        {
+            int y = Blue[0] * Blue[3] - Blue[1] * Blue[2];
+        }
+    }
+}
---
+++ Assets/Game/Source/Y_PAK/NonScripts/FunctionLibraries/RoundMove.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RoundMove 
+{
+    public static int row;
+    public static int column;
+
+    public static void Move()
+    {
+
+    }
+}
