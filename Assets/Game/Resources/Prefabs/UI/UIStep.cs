64704d5b7bcd38e5d796860174e04e8cb0fb8353
26384 <107617689+26384@users.noreply.github.com>
2022-12-04 01:39:04 +0800

    更新女生动画

--- Assets/Game/Resources/Prefabs/UI/UIStep.cs
+++ Assets/Game/Resources/Prefabs/UI/UIStep.cs
@@ -6,8 +6,16 @@ using UnityEngine.UI;
 
 public class UIStep : MonoBehaviour
 {
-    [SerializeField, DisplayName("角色血量")]
-    public Slider HP;
+    [SerializeField, DisplayName("Daven头像")]
+    public Sprite Daven;
+    [SerializeField, DisplayName("Lusii头像")]
+    public Sprite Lusii;
+
+    [SerializeField, DisplayName("Daven血量")]
+    public Slider DavenHP;
+    [SerializeField, DisplayName("Lusii血量")]
+    public Slider LusiiHP;
+
 
     [SerializeField, DisplayName("剩余步数")]
     public TMP_Text step;
